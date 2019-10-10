131db4efbe791ff8b8babdb277470fadb4253a1d
Daetheys <daetheys@gmail.com>
2019-10-10 23:31:45 +0200

    Finalisation de l'exo. Le coverage est bon (les seuls endroits qui n'ont pas le coverage parfait sont les tests au niveau des assert False). Je pense avoir suffisament bien testé les fonctions pour dire qu'elles sont sensées bien marcher et ne pas poser de problèmes.

--- bheap.py
+++ bheap.py
@@ -4,11 +4,15 @@
 #  - corriger les bugs;
 #  - envoyer le diff ou le dépôt git par email.
 
+class EmptyHeap(Exception):
+    pass
+
 class BinHeap:
     def __init__(self):
         self.heapList = [0]
         self.currentSize = 1
-
+        
+    #PRIVATE
     def percUp(self,i):
         while i // 2 > 0:
             if self.heapList[i] < self.heapList[i // 2]:
@@ -16,23 +20,27 @@ class BinHeap:
                 tmp = self.heapList[i // 2]
                 self.heapList[i // 2] = self.heapList[i]
                 self.heapList[i] = tmp
-            i = i // 2
-                    
+            	i = i // 2
+            else:
+            	break
+            	
     def insert(self,k):
-      self.heapList.append(k)
-      self.percUp(self.currentSize)
-      self.currentSize = self.currentSize + 1
+        self.heapList.append(k)
+        self.percUp(self.currentSize)
+        self.currentSize = self.currentSize + 1
 
+    #PRIVATE
     def percDown(self,i):
-      while (i * 2) < self.currentSize: #On verifie qu'on n'est pas sur une feuille
-          mc = self.minChild(i) #On prend le fils le plus petit
-          if self.heapList[i] > self.heapList[mc]:
-              #Swap
-              tmp = self.heapList[i]
-              self.heapList[i] = self.heapList[mc]
-              self.heapList[mc] = tmp
-          i = mc
+        while (i * 2) < self.currentSize: #On verifie qu'on n'est pas sur une feuille
+            mc = self.minChild(i) #On prend le fils le plus petit
+            if self.heapList[i] > self.heapList[mc]:
+                #Swap
+                tmp = self.heapList[i]
+                self.heapList[i] = self.heapList[mc]
+                self.heapList[mc] = tmp
+            i = mc
 
+    #PRIVATE
     def minChild(self,i):
         if i * 2 + 1 >= self.currentSize:
             return i * 2
@@ -43,26 +51,21 @@ class BinHeap:
               return i * 2 + 1
 
     def delMin(self):
-      retval = self.heapList[1]
-      self.currentSize = self.currentSize - 1
-      self.heapList[1] = self.heapList[self.currentSize]
-      self.heapList.pop()
-      self.percDown(1)
-      return retval
+        if self.currentSize <= 1:
+            raise EmptyHeap
+        retval = self.heapList[1]
+        self.currentSize = self.currentSize - 1
+        self.heapList[1] = self.heapList[self.currentSize]
+        self.heapList.pop()
+        self.percDown(1)
+        return retval
 
     def buildHeap(self,alist):
       #i = len(alist) #// 2
-      self.currentSize = len(alist)
+      self.currentSize = len(alist)+1
       self.heapList = [0] + alist[:]
       i = 1
-      while (i <= self.currentSize):
+      while (i <= self.currentSize-1):
           self.percUp(i)
           i = i + 1
 
-#Pour un debug plus précis et rapide, permet de faire des tests au cas par cas au fur et a mesure du lancement des fonctions
-if __name__ == "__main__":
-    B = BinHeap()
-    B.heapList = [0,0,-1,0,0,-1,0,0]
-    for i in range(1,8):
-        B.percUp(i)
-        print("--")
